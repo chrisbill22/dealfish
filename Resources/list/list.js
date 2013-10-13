5455984d836130809efce5ed60cb15aa840d516a
jmustaklem <jimustaklem@gmail.com>
2013-10-13 17:07:57 -0400

    More swipe

--- Resources/list/list.js
+++ Resources/list/list.js
@@ -1,2 +1,6 @@
 Ti.include("ui.js");
-
+Ti.include("navigation/navigation.js");
+listview.addEventListener('swipe', function(e){
+	listFront(getSlideDirection("list"));
+	alert('You swiped to the '+e.direction);
+});
--- Resources/search/search.js
+++ Resources/search/search.js
@@ -1,1 +1,6 @@
 Ti.include("ui.js");
+Ti.include("navigation/navigation.js");
+search_view.addEventListener('swipe', function(e){
+	searchFront(getSlideDirection("search"));
+	alert('You swiped to the '+ e.direction);
+});
