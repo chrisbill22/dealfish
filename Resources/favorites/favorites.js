31e76cb505fbd86af4caf60c9568fe6604abd4a2
jmustaklem <jimustaklem@gmail.com>
2013-10-14 10:38:41 -0400

    Testing

--- Resources/favorites/favorites.js
+++ Resources/favorites/favorites.js
@@ -1,5 +1,6 @@
 Ti.include("ui.js");
-Ti.include("navigation/navigation.js");
+/*
 favorites_view.addEventListener('swipe', function(){
 	favoritesFront(getSlideDirection("favorites"));
 });
+*/
--- Resources/search/search.js
+++ Resources/search/search.js
@@ -1,6 +1,7 @@
 Ti.include("ui.js");
-Ti.include("navigation/navigation.js");
+/*
 search_view.addEventListener('swipe', function(e){
 	searchFront(getSlideDirection("search"));
 	alert('You swiped to the '+ e.direction);
 });
+*/
