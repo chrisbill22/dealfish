6577843534d2e417edb78e9be636a852b65d1016
CB MacBook Pro <orangedog222@gmail.com>
2013-11-04 16:53:33 -0500

    Menu overlay zindex issue fix

--- Resources/company/ui.js
+++ Resources/company/ui.js
@@ -14,80 +14,86 @@ var backButton = Titanium.UI.createButton({
 	left: 0, 
 });
 function openCompany(id){
+	titleLabel.text = currentLocations[id][1];
+	distanceLabel.text = currentLocations[id][6];
+	descriptionLabel.text = currentLocations[id][0];
 	companyview.animate({
 		bottom: 0
 	});
-	var image = Ti.UI.createView({
-		width:'40%',
-		height:'20%',
-		backgroundColor:iOSBlue,
-		left:0, 
-		top: 90
-	});
-	var titleLabel = Titanium.UI.createLabel({
-		text: currentLocations[id][1],
-		width: '50%',
-		height: 50,
-		borderWidth: 0, 
-		top: 70, 
-		right: 10,
-	});
-	var distanceLabel = Titanium.UI.createLabel({
-		text: currentLocations[id][6],
-		width: '50%',
-		height: 50,
-		borderWidth: 0,
-		top: 100,
-		right: 10
-	});
-	var priceLabel = Titanium.UI.createLabel({
-		text: "DOLLAR SIGNS", 
-		width: '50%', 
-		height: 50, 
-		borderWidth: 0, 
-		top: 130,
-		right: 10
-	});
-	var descriptionLabel = Titanium.UI.createLabel({
-		text: currentLocations[id][0],
-		width: '75%', 
-		height: 50, 
-		borderWidth: 1, 
-		top: 225, 
-	});
-	var callButton = Titanium.UI.createButton({
-		title: "Call", 
-		width: 90, 
-		height: 90, 
-		borderWidth: 1, 
-		top: 325,
-		left: 10
-	});
-	var directionsButton = Titanium.UI.createButton({
-		title: "Directions", 
-		width: 90, 
-		height: 90, 
-		borderWidth: 1, 
-		top: 325,
-		left: 115
-	});
-	var favoritesButton = Titanium.UI.createButton({
-		title: "Add to Favorites", 
-		width: 90, 
-		height: 90, 
-		borderWidth: 1, 
-		top: 325,
-		right: 10, 
-		wordWrap: true
-	});
-	companyview.add(image);
-	companyview.add(titleLabel);
-	companyview.add(distanceLabel);
-	companyview.add(priceLabel);
-	companyview.add(descriptionLabel);
-	companyview.add(callButton);
-	companyview.add(directionsButton);
-	companyview.add(favoritesButton);
 }
+
+var image = Ti.UI.createView({
+	width:'40%',
+	height:'20%',
+	backgroundColor:iOSBlue,
+	left:0, 
+	top: 90
+});
+var titleLabel = Titanium.UI.createLabel({
+	text: '',
+	width: '50%',
+	height: 50,
+	borderWidth: 0, 
+	top: 70, 
+	right: 10,
+});
+var distanceLabel = Titanium.UI.createLabel({
+	text: '',
+	width: '50%',
+	height: 50,
+	borderWidth: 0,
+	top: 100,
+	right: 10
+});
+var priceLabel = Titanium.UI.createLabel({
+	text: "DOLLAR SIGNS", 
+	width: '50%', 
+	height: 50, 
+	borderWidth: 0, 
+	top: 130,
+	right: 10
+});
+var descriptionLabel = Titanium.UI.createLabel({
+	text: '',
+	width: '75%', 
+	height: 50, 
+	borderWidth: 1, 
+	top: 225, 
+});
+var callButton = Titanium.UI.createButton({
+	title: "Call", 
+	width: 90, 
+	height: 90, 
+	borderWidth: 1, 
+	top: 325,
+	left: 10
+});
+var directionsButton = Titanium.UI.createButton({
+	title: "Directions", 
+	width: 90, 
+	height: 90, 
+	borderWidth: 1, 
+	top: 325,
+	left: 115
+});
+var favoritesButton = Titanium.UI.createButton({
+	title: "Add to Favorites", 
+	width: 90, 
+	height: 90, 
+	borderWidth: 1, 
+	top: 325,
+	right: 10, 
+	wordWrap: true
+});
+companyview.add(image);
+companyview.add(titleLabel);
+companyview.add(distanceLabel);
+companyview.add(priceLabel);
+companyview.add(descriptionLabel);
+companyview.add(callButton);
+companyview.add(directionsButton);
+companyview.add(favoritesButton);
+
+
 companyview.add(backButton);
 MainWindow.add(companyview);
