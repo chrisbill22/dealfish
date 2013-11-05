1c9324f2ba330c7020c7a532c089f7f4741540af
jmustaklem <jimustaklem@gmail.com>
2013-11-04 19:54:32 -0500

    New settings view

--- Resources/settings/ui.js
+++ Resources/settings/ui.js
@@ -12,79 +12,35 @@ var settingsBack = Ti.UI.createButton({
 	bottom:5
 });
 var settingsTitle = Ti.UI.createView({
-	height:'12%',
+	height:'10%',
 	top:0,
 	left:0,
-	backgroundColor:'#DDD',
-	borderWidth:1
+	backgroundColor:'#FF6600',
 });
 var settingsTitle_label = Ti.UI.createLabel({
-	text:"Settings",
+	text:"Gear",
+	right: 10, 
+	bottom: 10
+});
+var data = [
+	{title: 'Notifications'}, 
+	{title: 'Set Location'},
+	{title: 'My Preferences'}, 
+	{title: 'Privacy Statement'}, 
+	{title: 'Terms of Use'}
+];
+var settingsTable = Ti.UI.createTableView({
+	data: data, 
+	top: 55, 
+	footerTitle: '', 
+	font:{fontFamily: 'Helvetica'},
 });
 
-var signup_button = Ti.UI.createButton({
-	top:'15%',
-	width:'100%',
-	zIndex:0,
-	backgroundColor:'#DDD',
-	title:"Sign Up", 
-	borderWidth: 1
-});
 
-var account_button = Ti.UI.createButton({
-	top:'20%',
-	width:'100%',
-	zIndex:0,
-	backgroundColor:'#DDD',
-	title:"Account", 
-	borderWidth: 1
-});
-
-var notifications_button = Ti.UI.createButton({
-	top:'25%',
-	width:'100%',
-	zIndex:0,
-	backgroundColor:'#DDD',
-	title:"Notifications", 
-	borderWidth: 1
-});
-
-var location_button = Ti.UI.createButton({
-	top:'30%',
-	width:'100%',
-	zIndex:0,
-	backgroundColor:'#DDD',
-	title:"Location", 
-	borderWidth: 1
-});
-
-var privacy_button = Ti.UI.createButton({
-	top:'45%',
-	width:'100%',
-	zIndex:0,
-	backgroundColor:'#DDD',
-	title:"Privacy Statement", 
-	borderWidth: 1
-});
-
-var terms_button = Ti.UI.createButton({
-	top:'55%',
-	width:'100%',
-	zIndex:0,
-	backgroundColor:'#DDD',
-	title:"Terms of Use", 
-	borderWidth: 1
-});
 
 settingsTitle.add(settingsTitle_label);
 settingsTitle.add(settingsBack);
 settings_view.add(settingsTitle);
-settings_view.add(signup_button);
-settings_view.add(account_button);
-settings_view.add(notifications_button);
-settings_view.add(location_button);
-settings_view.add(privacy_button);
-settings_view.add(terms_button);
-
+settings_view.add(settingsTable);
 
 MainWindow.add(settings_view);
